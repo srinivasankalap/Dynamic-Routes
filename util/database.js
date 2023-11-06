facafbab403e485ab80c068f05e0adcc4bf1aa59
srinivasankalap <cnu.ankalap@gmail.com>
2023-11-06 11:47:20 +0530

    Sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,21 @@
-const mysql=require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool=mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node_schema',
-    port: '3307',
-    password: '123456'
+const sequelize = new Sequelize("node_schema", "root", "123456", {
+  dialect: "mysql",
+  host: "localhost",
+  port: "3307",
 });
 
-module.exports=pool.promise();
+module.exports = sequelize;
+
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node_schema",
+//   port: "3307",
+//   password: "123456",
+// });
+
+// module.exports = pool.promise();
