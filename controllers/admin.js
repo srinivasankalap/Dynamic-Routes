2bdc1684db9133e08815b3d2a08a7c9fe9dc7735
srinivasankalap <cnu.ankalap@gmail.com>
2023-11-08 12:09:56 +0530

    Connecting databases

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,11 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
+    description: description,
   }).then(result=>{
     console.log(result);
     res.redirect("/");
@@ -32,7 +32,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findAll({where: {id: prodId}})
+  req.user.getProducts({where: {id: prodId}})
   .then(product=>{
     if (!product[0]) {
       return res.redirect("/");
@@ -65,7 +65,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
